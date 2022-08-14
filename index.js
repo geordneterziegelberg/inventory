0d72e013cc55860a7a49acb3e0245636551aa15e
geordneterziegelberg <sfg3@gmx.at>
2022-08-14 02:02:53 +0200

    html + findOne in analyticsDB

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const run = require('./database/connection.js')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.sendFile('/home/sebastian/Dev/inventory/index.html')
+})
+
+app.get('/add-user', (req, res)=>{
+  res.sendFile('/home/sebastian/Dev/inventory/add-user.html')
 })
 
 const server = app.listen(3000)
 
 run()
 
-console.log("leg")
+//console.log("leg")
 server.close()
